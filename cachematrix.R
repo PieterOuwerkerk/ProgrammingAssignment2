81e44e1fff5adb69c12544ac80fe893f15b1754d
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-04-22 10:09:22 -0400

    More typos

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These are functions to store a reversible matrix
 
-## Write a short comment describing this function
+## put the matrix in cache and clear its reverse and create the necessary functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    r <- NULL
+    set <- function(y) {
+        x <<- y
+        r <<- NULL
+    }
+    get <- function() x
+    setrev <- function(reverse) r <<- reverse
+    getrev <- function() r
+    list(set=set, get=get, setrev=setrev, getrev=getrev)
 }
 
 
-## Write a short comment describing this function
+## Retrieve the reverse matrix from cache if available
+## If it's not cached, calculate it first
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    tr <- x$getrev()
+    if (is.null(tr)) {
+        data <- x$get()
+        tr <- solve(data)
+        x$setrev(tr)
+    } else (
+        message("This one is from cache:")
+    )
+    tr
 }
